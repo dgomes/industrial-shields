39c7809890f6c06d37615f824748ca5fbdb2d337
Diogo Gomes <dgomes@av.it.pt>
2016-06-10 21:12:09 +0100

    support multiple PLC

--- src/relaybox.cpp
+++ src/relaybox.cpp
@@ -1,7 +1,20 @@
 #include "relaybox.h"
 
-RelayBox::RelayBox() {
+RelayBox::RelayBox(Model m) {
     this->callback = NULL;
+    switch(m) {
+        case UNDEF: 
+            relay = NULL;
+            relays_len = 0;
+            break;
+        case _34R:
+            relay = plc_m_duino_34R;
+            relays_len = sizeof(plc_m_duino_34R)/sizeof(int); 
+            break;
+        case _20IO:
+            relay = plc_ardbox_20IO_rele;
+            relays_len = sizeof(plc_ardbox_20IO_rele)/sizeof(int); 
+    }
 }
 
 void RelayBox::setup(RELAYBOX_CALLBACK_SIGNATURE) {
@@ -9,6 +22,9 @@ void RelayBox::setup(RELAYBOX_CALLBACK_SIGNATURE) {
 }
 
 void RelayBox::switchRelay(int i, bool mode) {  //true = HIGH, false = LOW
+    if(relay==NULL)
+        return;
+
     if(mode) {
         DEBUG_PRINTLN(String("Relay(")+String(i)+String(") ... ON"));
     } else {
@@ -32,8 +48,8 @@ void RelayBox::loop() {
 
     //keep track of time of last loop
     unsigned long elapsed_time = now - relay_sleep[0];
-    
-    for(int i=1; i< sizeof(relay)/sizeof(int); i++) {
+   
+    for(int i=1; i< relays_len; i++) {
         if(relay_sleep[i] != 0) {
             if((long) (relay_sleep[i] - elapsed_time) <= 0) {
                 switchRelay(i,false);
--- src/relaybox.h
+++ src/relaybox.h
@@ -4,7 +4,7 @@
  * This works with an M-DUINO PLC Arduino 38R http://www.industrialshields.com/m-duino-plc-arduino-34R-i-os-relay_analog-digital
  * and an EmonTX V3.4 (http://openenergymonitor.org/emon/modules/emonTxV3) connected through the Serial1 port
  * 
- * Relays are numbered:
+ * M-DUINO Relays are numbered:
  * 1 - R0.1       9  - R1.1
  * 2 - R0.2       10 - R1.2
  * 3 - R0.3       11 - R1.3
@@ -13,8 +13,16 @@
  * 6 - R0.6       14 - R1.6
  * 7 - R0.7       15 - R1.7
  * 8 - R0.8       16 - R1.8
- * 
- * Default configuration will listen for events on MQTT path /mduino/# where # is the number of the relay
+ *
+ * ARDBOX Relays are numbered:
+ * 1 - R1
+ * 2 - R2
+ * 3 - R3
+ * 4 - R4
+ * 5 - R5
+ * 6 - R6
+ * 7 - R7
+ * 8 - R8
  * Events can be a delay (in miliseconds) or the strings ON or OFF
  */
 #ifndef RelayBox_h
@@ -32,15 +40,21 @@
 
 #define RELAYBOX_CALLBACK_SIGNATURE void (*callback)(uint8_t, bool)
 
+static int plc_m_duino_34R[17] = {-1, 23,22,25,24,40,39,38,37,28,27,30,29,45,44,43,42}; //m-duino-plc-arduino-34R
+static int plc_ardbox_20IO_rele[9] = {-1, 4,7,8,9,10,11,12,13}; //ardbox-18ios-rele
+
+enum Model {UNDEF, _34R, _20IO};
+
 class RelayBox {
     private:
         //Configuration Relay OUT (220Vac - 8A max)
-        int relay[17] = {-1, 23,22,25,24,40,39,38,37,28,27,30,29,45,44,43,42}; //m-duino-plc-arduino-34R
+        int *relay;
+        size_t relays_len;
         unsigned long relay_sleep[17] = {1, 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0}; //index 0 hold last loop millis() 
         RELAYBOX_CALLBACK_SIGNATURE;
 
     public:
-        RelayBox(); //TODO box model/version as a enum parameter
+        RelayBox(Model m); //TODO box model/version as a enum parameter
         void setup(RELAYBOX_CALLBACK_SIGNATURE); 
 
         void switchRelay(int i, bool mode); //true = HIGH, false = LOW
