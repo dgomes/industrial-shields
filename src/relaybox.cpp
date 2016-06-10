1430875b37220455f37a676133d57fc0e0fa03c8
Diogo Gomes <dgomes@av.it.pt>
2016-06-10 15:27:11 +0100

    callback + better rollover handling

--- src/relaybox.cpp
+++ src/relaybox.cpp
@@ -1,6 +1,10 @@
 #include "relaybox.h"
 
-RelayBox::RelayBox(RELAYBOX_CALLBACK_SIGNATURE) {
+RelayBox::RelayBox() {
+    this->callback = NULL;
+}
+
+void RelayBox::setup(RELAYBOX_CALLBACK_SIGNATURE) {
     this->callback = callback;
 }
 
@@ -11,31 +15,34 @@ void RelayBox::switchRelay(int i, bool mode) {  //true = HIGH, false = LOW
         DEBUG_PRINTLN(String("Relay(")+String(i)+String(") ... OFF"));
     }
     digitalWrite(relay[i],mode?HIGH:LOW);
-    this->callback(i, mode);
+    if(this->callback)
+        this->callback(i, mode);
 }
 
-// Program relay to turn off in the future
+// Turn relay ON and program relay to turn OFF in the future
 void RelayBox::switchRelay(int i, int period) {
-    unsigned long now = millis();
-    
     switchRelay(i,true);
     
-    relay_sleep[i] = now + period;
+    relay_sleep[i] = period;
 }
 
-//Switch off after period
+//Switch AFTER after period
 void RelayBox::loop() {
     unsigned long now = millis();
 
     //keep track of time of last loop
-    if(now < relay_sleep[0]) {
-        relay_sleep[0] = now;
-    }
-
-    for(int i=1; i<= MAX_RELAY; i++) {
-        if (relay_sleep[i] != 0 && now >= relay_sleep[i]) {
-            switchRelay(i,false);
-            relay_sleep[i] = 0;
+    unsigned long elapsed_time = now - relay_sleep[0];
+    
+    for(int i=1; i< sizeof(relay)/sizeof(int); i++) {
+        if(relay_sleep[i] != 0) {
+            if((long) (relay_sleep[i] - elapsed_time) <= 0) {
+                switchRelay(i,false);
+                relay_sleep[i] = 0;
+            } else {
+                relay_sleep[i] -= elapsed_time;
+            }
         }
     }
+
+    relay_sleep[0] = now;
 }
--- src/relaybox.h
+++ src/relaybox.h
@@ -1,3 +1,22 @@
+/*
+ * Author: Diogo Gomes <diogogomes@gmail.com>
+ * 
+ * This works with an M-DUINO PLC Arduino 38R http://www.industrialshields.com/m-duino-plc-arduino-34R-i-os-relay_analog-digital
+ * and an EmonTX V3.4 (http://openenergymonitor.org/emon/modules/emonTxV3) connected through the Serial1 port
+ * 
+ * Relays are numbered:
+ * 1 - R0.1       9  - R1.1
+ * 2 - R0.2       10 - R1.2
+ * 3 - R0.3       11 - R1.3
+ * 4 - R0.4       12 - R1.4
+ * 5 - R0.5       13 - R1.5
+ * 6 - R0.6       14 - R1.6
+ * 7 - R0.7       15 - R1.7
+ * 8 - R0.8       16 - R1.8
+ * 
+ * Default configuration will listen for events on MQTT path /mduino/# where # is the number of the relay
+ * Events can be a delay (in miliseconds) or the strings ON or OFF
+ */
 #ifndef RelayBox_h
 #define RelayBox_h
 
@@ -11,19 +30,18 @@
 #define DEBUG_PRINTLN(x) 
 #endif
 
-//Configuration Relay OUT (220Vac - 8A max)
-#define MAX_RELAY 16
-
 #define RELAYBOX_CALLBACK_SIGNATURE void (*callback)(uint8_t, bool)
 
 class RelayBox {
     private:
-        int relay[17] = {-1, 23,22,25,24,40,39,38,37,28,27,30,29,45,44,43,42};
+        //Configuration Relay OUT (220Vac - 8A max)
+        int relay[17] = {-1, 23,22,25,24,40,39,38,37,28,27,30,29,45,44,43,42}; //m-duino-plc-arduino-34R
         unsigned long relay_sleep[17] = {1, 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0}; //index 0 hold last loop millis() 
         RELAYBOX_CALLBACK_SIGNATURE;
 
     public:
-        RelayBox(RELAYBOX_CALLBACK_SIGNATURE); //TODO box model/version as a enum parameter
+        RelayBox(); //TODO box model/version as a enum parameter
+        void setup(RELAYBOX_CALLBACK_SIGNATURE); 
 
         void switchRelay(int i, bool mode); //true = HIGH, false = LOW
         void switchRelay(int i, int period); // Program relay to turn off in the future - milliseconds
